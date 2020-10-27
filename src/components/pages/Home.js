0fc805d2b815aeaa6e0456bf52d1d6a083c7a31a
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-10-27 10:57:41 -0500

    added multiple component sna files to customer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import BG from '../../images/BG.jpeg';
+import {Link} from 'react-router-dom'
+import HomeSection from './Homefiles/HomeSection'
 
 function Home() {
   return (
@@ -14,7 +16,7 @@ function Home() {
         </div>
         <div className="home-desc-flex">
           <div className="home-desc-box">
-            <div>
+            <div className="home-desc-summary">
               <p>
                 AgroAmerica grows, ships and distributes bananas and vegetable oil. We strive to be a sustainable agriculture business committed to improving the lives of our employees while protecting the environment. This commitment to sustainable agriculture and responsible business practices is a core element in all our operations in Central and South America, the United States, UK and Europe.
               </p>
@@ -28,14 +30,14 @@ function Home() {
             <div className="home-desc-image">
               <img src={BG} alt='logo' />
             </div>
-            <div>
+            <div className="home-desc-summary">
               <p>
                 Since our inception in 1958 we’ve become a leading sustainable agriculture business in Central and South America. We’ve grown from one small banana plantation to an international agribusiness corporation.  As a sustainable agriculture business we’re committed to the development of the countries where we operate, as well as to the protection and conservation of the environment.
               </p>
             </div>
           </div>
           <div className="home-desc-box">
-            <div>
+            <div  className="home-desc-summary">
               <p>
                 Our goals are not just focused on the production of bananas and sustainable tropical oil in Guatemala, Panama, Costa Rica, Ecuador and Peru. We’re also deeply committed to the welfare and development of the people we employ, their families and the communities near our operations.
               </p>
@@ -44,6 +46,14 @@ function Home() {
               <img src={BG} alt='logo' />
             </div>
           </div>
+        </div>
+        <div className="home-bottom-summary">
+          <p>
+          Besides being a competitive agribusiness corporation, through our commitment to responsible farming and protection of the environment we’re also known as an advocate for sustainable agriculture. We’re actively involved in improving the welfare of our employees, the communities around our operations and conservation of the environment and have many social programs that assist our people and the communities close to our operations. AgroAmerica is a member of the Better Banana Project (BBP) and we are certified by the Rainforest Alliance, Global Gap, SCS Sustainably Grown and BASC.
+          </p>
+        </div>
+        <div>
+          <HomeSection />
         </div>
       </div>
     </div>
